119e30ce16bd6c92e61edd423755c1b0e84e027b
geek-mayank <mayankdilipgupta11@gmail.com>
2025-09-13 12:01:55 +0530

    Hyperparameter Tuning

--- src/utils.py
+++ src/utils.py
@@ -11,30 +11,33 @@ from sklearn.model_selection import GridSearchCV
 from src.exception import CustomException
 
 def save_object(file_path, obj):
-  try:
-    dir_path = os.path.dirname(file_path)
+    try:
+        dir_path = os.path.dirname(file_path)
 
-    os.makedirs(dir_path, exist_ok=True)
+        os.makedirs(dir_path, exist_ok=True)
 
-    with open(file_path, "wb") as file_obj:
-      dill.dump(obj, file_obj)
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
 
-  except Exception as e:
-    raise CustomException(e, sys)
-
-def evaluate_models(X_train, y_train,X_test,y_test,models):
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            #para=param[list(models.keys())[i]]
+            para=param[list(models.keys())[i]]
 
-            ##gs = GridSearchCV(model,para,cv=3)
-            #gs.fit(X_train,y_train)
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
 
-            #model.set_params(**gs.best_params_)
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
+
             y_train_pred = model.predict(X_train)
 
             y_test_pred = model.predict(X_test)
@@ -48,4 +51,12 @@ def evaluate_models(X_train, y_train,X_test,y_test,models):
         return report
 
     except Exception as e:
-      raise CustomException(e,sys)
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
